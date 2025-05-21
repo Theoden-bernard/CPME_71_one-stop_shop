8b9e6d88b9102fb8d3832e2fdb8e23b02875648b
Bernard Théoden <theodenbernard1203@gmail.com>
2025-05-21 21:30:16 +0200

    FEAT: change login and change dysplay and remove registration

--- lib/service_desk_web/live/user_confirmation_live.ex
+++ lib/service_desk_web/live/user_confirmation_live.ex
@@ -16,7 +16,6 @@ defmodule ServiceDeskWeb.UserConfirmationLive do
       </.simple_form>
 
       <p class="text-center mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
         | <.link href={~p"/users/log_in"}>Log in</.link>
       </p>
     </div>
--- lib/service_desk_web/live/user_login_live.ex
+++ lib/service_desk_web/live/user_login_live.ex
@@ -8,9 +8,7 @@ defmodule ServiceDeskWeb.UserLoginLive do
         Log in to account
         <:subtitle>
           Don't have an account?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
             Sign up
-          </.link>
           for an account now.
         </:subtitle>
       </.header>
