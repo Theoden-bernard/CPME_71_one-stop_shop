95076b6dc81f185fb1113f92911ba7a6ee0316ff
Bernard Théoden <theodenbernard1203@gmail.com>
2025-05-25 23:20:45 +0200

    FEAT: add delete fonction

--- lib/service_desk/messages.ex
+++ lib/service_desk/messages.ex
@@ -6,7 +6,7 @@ defmodule ServiceDesk.Messages do
     def get_message!(id), do: Repo.get!(Message, id)
 
     def list_messages do
-        
+        # acceder a la db et aficher tout le contenue
     end
 
     def create_message(%{"user_id" => user_id} = attrs) when is_integer(user_id) and user_id > 0 do
@@ -19,11 +19,15 @@ defmodule ServiceDesk.Messages do
         
     end
     
-    def update_message(_message, _attrs) do
-        
+    def update_message(message, attrs) do
+        message
+        |> Message.changeset(attrs)
+        |> Repo.update()
     end
 
-    def delete_message(_message) do
-        
+    def delete_message(message) do
+        # acceder a la db puis chercher le message grace a la variable messages et suprimer l'element
+        Repo.delete_all(message)
+        :ok
     end
 end
